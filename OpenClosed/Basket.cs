3762450b6c143e5bd3b4557e6e4159560d681f4a
Каюн Максим <maks.kayun@yandex.ru>
2023-10-07 13:20:00 +0300

    add open-closed principle

---
+++ OpenClosed/Basket.cs
@@ -0,0 +1,30 @@
+﻿namespace OpenClosed;
+
+public class Basket
+{
+    private List<PeriodicalProduct> PeriodicalProducts { get; set; } = new();
+    private List<OneTimeProduct> OneTimeProducts { get; set; } = new();
+
+    private void AddOneTimeProduct(OneTimeProduct product)
+    {
+        OneTimeProducts.Add(product);
+    }
+
+    private void AddPeriodicalProduct(PeriodicalProduct product)
+    {
+        PeriodicalProducts.Add(product);
+        AddOneTimeProduct(product.OtProduct);
+    }
+
+    public void AddProduct(object product)
+    {
+        if (product is PeriodicalProduct perProd)
+        {
+            AddPeriodicalProduct(perProd);
+        } 
+        else if (product is OneTimeProduct otProd)
+        {
+            AddOneTimeProduct(otProd);
+        }
+    }
+}
---
+++ OpenClosed/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenClosed;
+
+var otProd = new OneTimeProduct();
+var perProd = new PeriodicalProduct
+{
+    OtProduct = otProd
+};
+
+var otProd2 = new OneTimeProduct();
+
+var basket = new Basket();
+basket.AddProduct(perProd);
+basket.AddProduct(otProd2);
