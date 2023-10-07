14f445cdd90c79b183b3ba5f630ea6f454c8a519
Каюн Максим <maks.kayun@yandex.ru>
2023-10-07 12:42:50 +0300

    add single responsibility principle

---
+++ SingleResponsibility/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace SingleResponsibility;
+
+public class Product
+{
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public double CalculateTotalPrice(int quantity)
+    {
+        if (quantity <= 0)
+        {
+            throw new Exception("Количество товара должно быть положительным числом.");
+        }
+
+        return Price * quantity;
+    }
+}
---
+++ SingleResponsibility/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SingleResponsibility;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var product = new Product(name: "Книга", price: 10.0);
+
+        Console.WriteLine($"Имя товара: {product.Name}");
+        Console.WriteLine($"Цена товара: {product.Price}");
+        Console.WriteLine($"Итоговая цена за 5 единиц: {product.CalculateTotalPrice(5)}");
+    }
+}
