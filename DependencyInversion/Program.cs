313f3c4b119cc087c9df79d151fe465a5db0699d
Каюн Максим <maks.kayun@yandex.ru>
2023-10-16 18:35:59 +0300

    fix & add puml

--- DependencyInversion/Program.cs
+++ DependencyInversion/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using DependencyInversion.Services;
-using DependencyInversion.Implementation;
 
 namespace DependencyInversion;
 
--- DependencyInversion/RepositoryCreator.cs
+++ DependencyInversion/RepositoryCreator.cs
@@ -2,7 +2,7 @@
 
 namespace DependencyInversion;
 
-public class RepositoryCreator
+public static class RepositoryCreator
 {
     public static IRepository CreateRepository(bool usePg) =>
         usePg ? new PgRepository() : new OraRepository();
